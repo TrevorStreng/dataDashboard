6ba33eb84be4aa10e30c9afe4bad04aee0a6d060
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-03-07 14:02:08 -0800

    wrote function for most sold items

--- backend/dataProcessing.js
+++ backend/dataProcessing.js
@@ -9,7 +9,7 @@ const { finished } = require("stream");
 //   .on("data", (data) => results.push(data))
 //   .on("end", () => console.log(results));
 
-const df = fs.createReadStream("backend/csv/data.csv");
+const df = fs.createReadStream("csv/data.csv");
 
 // console.log(df);
 async function totalSalesAmount() {
@@ -55,6 +55,67 @@ async function profitOverTime() {
   });
   return profits;
 }
+
+async function topProducts() {
+  const productArr = [];
+  const productCnt = [];
+  await new Promise((resolve, reject) => {
+    df.pipe(csv())
+      .on("data", (row) => {
+        if (!productArr.includes(row.Description)) {
+          productArr.push(row.Description);
+          productCnt.push(1); // product count is at the same index as the product in productArr
+        } else {
+          const arrIndex = productArr.indexOf(row.Description);
+          productCnt[arrIndex]++;
+        }
+      })
+      .on("end", () => {
+        // find the index of the top 3-5 products
+        const mostSold = [];
+        const numOfTopProducts = 5;
+        for (let i = 0; i < numOfTopProducts; i++) {
+          // init most sold to all 0's
+          mostSold.push(0);
+        }
+        for (let i = 0; i < productCnt.length; i++) {
+          let mostSoldSearchIndex = numOfTopProducts - 1;
+          while (mostSoldSearchIndex >= 0) {
+            if (productCnt[i] > mostSold[mostSoldSearchIndex]) {
+              if (mostSoldSearchIndex === 0) {
+                // inserts first value
+                console.log("made it here");
+                mostSold.splice(mostSold[mostSoldSearchIndex - 1], 0, i - 1);
+                mostSold.pop();
+                mostSoldSearchIndex = -1; // this exits the while loop
+              }
+              mostSoldSearchIndex--;
+            } else if (
+              productCnt[i] < mostSold[mostSoldSearchIndex] &&
+              productCnt[i] > mostSold[mostSoldSearchIndex - 1]
+            ) {
+              console.log("made it here");
+              mostSold.splice(mostSold[mostSoldSearchIndex - 1], 0, i - 1);
+              mostSold.pop();
+              mostSoldSearchIndex = -1; // this exits the while loop
+            } else {
+              mostSoldSearchIndex = -1; // this exits the while loop
+            }
+            // console.log(mostSoldSearchIndex);
+          }
+          console.log(mostSold);
+        }
+        let topProductArr = [];
+        for (let i = 0; i < numOfTopProducts; i++) {
+          topProductArr.push(productArr[mostSold[i]]);
+        }
+        resolve(topProductArr);
+        console.log(topProductArr);
+      });
+  });
+}
+
+topProducts();
 
 // make graph of income over time next
 
